bb305d9df8b57563cf7ea283a6f44c6ecbdc34e5
park <gwgtis135@gmail.com>
2021-09-02 10:31:44 +0900

    ll

--- 20210901/src/co/yedam/bookmanagement/BookDAO.java
+++ 20210901/src/co/yedam/bookmanagement/BookDAO.java
@@ -135,6 +135,45 @@ private Connection getConnection() {
 			
 			
 		}
+		
+		public void rent() {
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			String sql = "";
+			ResultSet rs = null;
+			try {
+				conn  = this.getConnection();
+				sql = "INSERT INTO book_rent VALUES(001, 'aaa','a001',SYSDATE,'N');";
+				pstmt = conn.prepareStatement(sql);         
+				pstmt.setString(1,name);
+				rs = pstmt.executeQuery();
+//				
+//				while(rs.next()) {
+//					Book book = new Book();
+//					book.setIsbn(rs.getString("isbn"));
+//					book.setName(rs.getString("name"));
+//					book.setContent(rs.getString("content"));
+//				}
+				if(rs !=null) {
+				while(rs.next()) {		
+					System.out.print("BOOK_CODE "+ rs.getString("BOOK_CODE")); 
+		    		System.out.print("\tBOOK_TITLE: "+ rs.getString("BOOK_TITLE"));
+		    		System.out.print("\tBOOK_AUTHOR: "+ rs.getString("BOOK_AUTHOR"));
+		    		System.out.print("\t개요: "+ rs.getString("BOOK_COMENT"));
+		    		
+		    		System.out.println("search success");
+		    		}
+				}
+				else {
+					System.out.println("serach false  ");
+				}
+							
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				closeJDBC(conn, pstmt, rs);
+			}
+		}
 	
 	
 }
